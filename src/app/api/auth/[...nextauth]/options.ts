60f1a8643796d13ba8093cc1e75c072b7234cf17
EdiGal <edi@securitize.io>
2023-09-02 12:43:57 +0300

    protected dashboard by middleware

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,43 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "email and password",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email) return null;
+        if (!credentials?.password) return null;
+
+        interface IUser {
+          id: string;
+          email: string;
+          password?: string;
+        }
+
+        // have a user
+        const user: IUser = {
+          id: "123",
+          email: "edi@sec.gov",
+          password: "asd123",
+        };
+
+        const isEmailOk = credentials.email === user.email;
+        const isPassOk = credentials.password === user.password;
+        if (isEmailOk && isPassOk) {
+          delete user.password;
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
